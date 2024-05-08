bcd6005c0629cff8ab68880dfda089e6371a2ab5
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-05-08 15:21:07 +0200

    Renamed other variables

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 // Renamed the variables to make it more descriptive
 const velocity = { value: 10000, unit: 'km/h' }; // velocity (km/h)
 const acceleration = { value: 3, unit: 'm/s^2' }; // acceleration (m/s^2)
+const duration = { value: 3600, unit: 's' }; // seconds (1 hour)
+const initialDistance = { value: 0, unit: 'km' }; // distance (km)
+const fuelAmount = { value: 5000, unit: 'kg' }; // remaining fuel (kg)
+const fuelBurnRate = { value: 0.5, unit: 'kg/s' }; // fuel burn rate (kg/s)
+
